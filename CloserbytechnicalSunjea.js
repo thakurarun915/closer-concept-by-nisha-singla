72aa57ba26a047ffd0213ea88a21d7567cf0ab46
Arun Singh Rajpoot <rajpoot9897arun@gmail.com>
2024-07-18 00:59:23 -0700

    this for closer concept

--- CloserbytechnicalSunjea.js
+++ CloserbytechnicalSunjea.js
@@ -1,6 +1,14 @@
 //closer in javascript by te hnical suneja
 var sun =function(a){
     console.log("Hello Viewrs"+a);
+    var c=4;
+    return function(b){
+        return a+b+c;
+
+    }
 
 }
-sun(2);
+
+var store =sun(2);//calling sun function this is for me var varib ales has funcnction scope and global and can we run from any  other 
+
+console.log(store(5));
